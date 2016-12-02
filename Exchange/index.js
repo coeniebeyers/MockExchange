9ceb10d6b584f424e68504f29b361df615199164
Peter Munnings <petermunnings@gmail.com>
2016-12-02 12:40:57 +0200

    Added the Exchange webservice

---
+++ Exchange/index.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.listen(3033, function () {
+  console.log('Exchange engine is running on port 3033')
+})
