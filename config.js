cd9dbcd1304732e678063cfe6211f4b2c2dc3311
Coenie <coeniebeyers@gmail.com>
2016-12-08 13:22:41 +0200

    Moved account functions to its own file.

--- config.js
+++ config.js
@@ -2,5 +2,15 @@ var config = {};
 
 config.dbConnectionString = 'exchange';
 config.dbCollections = ['orderLog', 'userRegistry'];
+config.currency1 = {
+  name: 'BTC',
+  decimals: 10,
+  constant: Math.pow(10, 10)
+};
+config.currency2 = {
+  name: 'USD',
+  decimals: 4,
+  constant: Math.pow(10, 4)
+};
 
 module.exports = config;
