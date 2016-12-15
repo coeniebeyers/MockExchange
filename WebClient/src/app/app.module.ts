0a8ff8439141f0aafb2c49b79b0aa0be4ce0fbb3
Peter Munnings <petermunnings@gmail.com>
2016-12-15 08:17:52 +0200

    Working on getting login and registration working.  Need to prune a lot of unused bits and pieces still

--- WebClient/src/app/app.module.ts
+++ WebClient/src/app/app.module.ts
@@ -4,13 +4,26 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { GoogleChartComponent } from './google-chart/google-chart.component';
+
+import { AlertComponent } from './_directives/index';
+import { AuthGuard } from './_guards/index';
+import { AlertService, AuthenticationService, UserService } from './_services/index';
+import { HomeComponent } from './home/index';
+import { LoginComponent } from './login/index';
+import { RegisterComponent } from './register/index';
+
+
 import { DropdownModule } from 'ng2-bootstrap/ng2-bootstrap';
 import { ModalModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GoogleChartComponent
+    GoogleChartComponent,
+    AlertComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +33,13 @@ import { ModalModule } from 'ng2-bootstrap/ng2-bootstrap';
     ModalModule
   ],
   providers: [ 
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
