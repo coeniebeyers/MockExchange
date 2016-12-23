32418a7afbf8521671e75f3156264604d5239b5f
Coenie Beyers <coeniebeyers@gmail.com>
2016-12-23 10:31:47 +0200

    Added test for BTC and USD account balance updates

--- test/accountManagement.js
+++ test/accountManagement.js
@@ -16,11 +16,31 @@ describe('Account', function() {
       done();
     });
   });
-  /*it('should be able to update account balances', function(done){
-    
-    done();
+  it('should be able to adjust BTC account balances', function(done){
+    var obj = {
+      accountId: accountList[0].id,
+      amount: 100,
+      currency: 'BTC'
+    };
+    accountManagement.AdjustAccountBalance(obj, function(accountBalance){
+      expect(accountBalance.BTC).to.equal(obj.amount);
+      expect(accountBalance.USD).to.equal(0);
+      done();
+    });
   });
-  it('should be able to update account balances', function(done){
+  it('should be able to adjust USD account balances', function(done){
+    var obj = {
+      accountId: accountList[0].id,
+      amount: 1000,
+      currency: 'USD'
+    };
+    accountManagement.AdjustAccountBalance(obj, function(accountBalance){
+      expect(accountBalance.BTC).to.equal(100);
+      expect(accountBalance.USD).to.equal(obj.amount);
+      done();
+    });
+  });
+  /*it('should be able to update account balances', function(done){
     
     done();
   });*/
