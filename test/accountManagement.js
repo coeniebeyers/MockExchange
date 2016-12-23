a73bfd5604ee03d9fba35e0c61aa30595f42e487
Coenie Beyers <coeniebeyers@gmail.com>
2016-12-23 09:40:20 +0200

    Changed version of anguler-cli in WebClient/package.json. Added test for getting balance from account management

--- test/accountManagement.js
+++ test/accountManagement.js
@@ -9,7 +9,19 @@ describe('Account', function() {
     accountList.push(account);
     done();
   });
-  it('should be able to add an account', function(done){
+  it('should be able to return the balance of an account', function(done){
+    accountManagement.GetAccountBalances(accountList[0].id, function(accountBalance){ 
+      expect(accountBalance.BTC).to.equal(0);
+      expect(accountBalance.USD).to.equal(0);
+      done();
+    });
+  });
+  /*it('should be able to update account balances', function(done){
+    
     done();
   });
+  it('should be able to update account balances', function(done){
+    
+    done();
+  });*/
 });
