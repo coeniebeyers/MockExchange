e53cc43a0a0c0a09f4a75e4bbcb81002f1ffb646
Coenie Beyers <coeniebeyers@gmail.com>
2017-01-04 12:45:21 +0200

    Added test for updating reserved amounts for asks

--- test/accountManagement.js
+++ test/accountManagement.js
@@ -105,4 +105,35 @@ describe('Account', function() {
     expect(balances2.USD).to.be(900);
     done();
   });
+  it('should be able to update reserved balances on a matching ask', function(done){
+    var oldBalancesAccount1 = accountManagement.GetAccountBalances(accountList[0].id);
+    var oldBalancesAccount2 = accountManagement.GetAccountBalances(accountList[1].id);
+    var match = {
+      order1: {
+        timestamp: 1481536182350,
+        id: '4e374320-c050-11e6-992a-730712547a2a',
+        accountId: accountList[0].id,
+        type: 'ask',
+        price: 10,
+        amount: 20 
+      },
+      order2: {
+        timestamp: 1481536182351,
+        id: '4e374323-c050-11e6-992a-730712547a2a',
+        accountId: accountList[1].id,
+        type: 'bid',
+        price: 10,
+        amount: 20 },
+      amount: 20
+    };    
+    accountManagement.UpdateAccountBalances(match);
+    var balances1 = accountManagement.GetAccountBalances(accountList[0].id);
+    expect(balances1.accountId).to.be(0);
+    expect(balances1.reservedBTC).to.be(oldBalancesAccount1.reservedBTC - match.amount);
+
+    var balances2 = accountManagement.GetAccountBalances(accountList[1].id);
+    expect(balances2.accountId).to.be(1);
+    expect(balances2.reservedUSD).to.be(oldBalancesAccount2.reservedUSD - match.amount * match.order1.price);
+    done();
+  });
 });
