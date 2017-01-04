f2b30aa56fecc06b8e8319e63a0711146976e72c
Coenie Beyers <coeniebeyers@gmail.com>
2017-01-04 12:44:49 +0200

    Fixed allocation of reserved amounts for asks

--- Exchange/accountManagement.js
+++ Exchange/accountManagement.js
@@ -2,75 +2,75 @@ var config = require('../config.js');
 var util = require('../util.js');
 
 var accountList = [];
+function updateReserveAmountsByParams(accountId, amount, price, orderType){
 
-function updateReserveAmounts(order){
-  var account = accountList[order.accountId];
-  if(order.type == 'ask'){
-    var amount = order.amount;
+  var account = accountList[accountId];
+  if(orderType == 'ask'){
     account.reservedCurrency1 += amount;
     account.reservedCurrency1 = util.Round(account.reservedCurrency1, config.currency1.constant);
-  } else if(order.type == 'cancelask'){
-    var amount = order.amount;
+  } else if(orderType == 'cancelask'){
     account.reservedCurrency1 -= amount;
     account.reservedCurrency1 = util.Round(account.reservedCurrency1, config.currency1.constant);
-  } else if(order.type == 'updateask'){
-    var amount = order.amount;
-    account.reservedCurrency1 += amount;
+  } else if(orderType == 'matchedask'){
+    account.reservedCurrency1 -= amount;
     account.reservedCurrency1 = util.Round(account.reservedCurrency1, config.currency1.constant);
-  } else if(order.type == 'bid'){
-    var amount = order.amount * order.price;
-    account.reservedCurrency2 += amount;
+  }  else if(orderType == 'bid'){
+    var bidAmount = amount * price;
+    account.reservedCurrency2 += bidAmount;
     account.reservedCurrency2 = util.Round(account.reservedCurrency2, config.currency2.constant);
-  } else if(order.type == 'cancelbid'){
-    var amount = order.amount * order.price;
-    account.reservedCurrency2 -= amount;
+  } else if(orderType == 'cancelbid'){
+    var bidAmount = amount * price;
+    account.reservedCurrency2 -= bidAmount;
     account.reservedCurrency2 = util.Round(account.reservedCurrency2, config.currency2.constant);
-  } else if(order.type == 'updatebid'){
-    var amount = order.amount * order.price;
-    account.reservedCurrency2 += amount;
+  } else if(orderType == 'matchedbid'){
+    var bidAmount = amount * price;
+    account.reservedCurrency2 -= bidAmount;
     account.reservedCurrency2 = util.Round(account.reservedCurrency2, config.currency2.constant);
-  }
+  } 
+}
+
+function updateReserveAmounts(order){
+  updateReserveAmountsByParams(order.accountId, order.amount, order.price, order.type);
 }
 
 function updateAccountBalances(match){
   var currency1Amount = match.amount;
-  //console.log('currency1Amount:', currency1Amount);
-  var currency2Amount = match.order1.price * currency1Amount;
-  //console.log('currency2Amount:', currency2Amount);
+  var price = match.order1.price;
+  var currency2Amount = price * currency1Amount;
   if(match.order1.type == 'ask'){
     var account1 = accountList[match.order1.accountId];
     account1.currency1 -= currency1Amount;
     account1.currency1 = util.Round(account1.currency1, config.currency1.constant);
-    account1.reservedCurrency1 -= currency1Amount;
-    account1.reservedCurrency1 = util.Round(account1.reservedCurrency1, config.currency1.constant);
     account1.currency2 += currency2Amount;
     account1.currency2 = util.Round(account1.currency2, config.currency2.constant);
+    updateReserveAmountsByParams(account1.id, currency1Amount, price, 'matchedask');
 
     var account2 = accountList[match.order2.accountId];
     account2.currency1 += currency1Amount;
     account2.currency1 = util.Round(account2.currency1, config.currency1.constant);
     account2.currency2 -= currency2Amount;
     account2.currency2 = util.Round(account2.currency2, config.currency2.constant);
-    account2.reservedCurrency2 -= match.order2.price*currency1Amount;
-    account2.reservedCurrency2 = util.Round(account2.reservedCurrency2, config.currency2.constant);
+    updateReserveAmountsByParams(account2.id, currency1Amount, price, 'matchedbid');
   } else if(match.order1.type == 'bid') { 
     var account1 = accountList[match.order1.accountId];
     account1.currency1 += currency1Amount;
     account1.currency1 = util.Round(account1.currency1, config.currency1.constant);
+    account1.reservedCurrency1 += currency1Amount;
+    account1.reservedCurrency1 = util.Round(account1.reservedCurrency1, config.currency1.constant);
 
     account1.currency2 -= currency2Amount;
     account1.currency2 = util.Round(account1.currency2, config.currency2.constant);
-
     account1.reservedCurrency2 -= currency2Amount;
     account1.reservedCurrency2 = util.Round(account1.reservedCurrency2, config.currency2.constant);
 
     var account2 = accountList[match.order2.accountId];
     account2.currency1 -= currency1Amount;
     account2.currency1 = util.Round(account2.currency1, config.currency1.constant);
+    account2.reservedCurrency1 -= currency1Amount;
+    account2.reservedCurrency1 = util.Round(account2.reservedCurrency1, config.currency1.constant);
 
     account2.reservedCurrency1 -= currency1Amount;
     account2.reservedCurrency1 = util.Round(account2.reservedCurrency1, config.currency1.constant);
-    
     account2.currency2 += currency2Amount;
     account2.currency2 = util.Round(account2.currency2, config.currency2.constant);
   } 
@@ -182,7 +182,9 @@ function getAccountBalances(accountId, cb){
     accountId: accountId
   };
   balanceObj[config.currency1.name] = account.currency1; 
+  balanceObj['reserved'+config.currency1.name] = account.reservedCurrency1; 
   balanceObj[config.currency2.name] = account.currency2; 
+  balanceObj['reserved'+config.currency2.name] = account.reservedCurrency2; 
   if(cb){
     cb(balanceObj);
   } else {
@@ -196,11 +198,9 @@ function adjustAccountBalance(obj, cb){
   if(obj.currency == config.currency1.name){
     var adjustment = util.Round(Number(obj.amount), config.currency1.constant);
     account.currency1 += adjustment;
-    account.reservedCurrency1 += adjustment;
   } else if (obj.currency == config.currency2.name){
     var adjustment = util.Round(Number(obj.amount), config.currency2.constant);
     account.currency2 += adjustment;
-    account.reservedCurrency2 += adjustment;
   } 
  
   var balanceObj = getAccountBalances(obj.accountId);
