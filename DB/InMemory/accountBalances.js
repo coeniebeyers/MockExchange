9d76e5ff0d3568e8ab0498dc1ce2251ea493eace
Coenie Beyers <coeniebeyers@gmail.com>
2017-01-10 14:22:57 +0200

    Added optional callbacks.

--- DB/InMemory/accountBalances.js
+++ DB/InMemory/accountBalances.js
@@ -3,29 +3,45 @@ var util = require('../../util.js');
 
 var accountList = [];
 
-function addToReservedCurrency1(accountId, amount){
+function addToReservedCurrency1(accountId, amount, cb){
   var account = accountList[accountId];
   account.reservedCurrency1 += amount;
   account.reservedCurrency1 = util.Round(account.reservedCurrency1, config.currency1.constant);
+  if(cb){
+    cb(account);
+  }
 }
 
-function removeFromReservedCurrency1(accountId, amount){
-  addToReservedCurrency1(accountId, -1*amount); 
+function removeFromReservedCurrency1(accountId, amount, cb){
+  addToReservedCurrency1(accountId, -1*amount, function(res){
+    if(cb){
+      cb(res);
+    }
+  }); 
 }
 
-function addToReservedCurrency2(accountId, amount){
+function addToReservedCurrency2(accountId, amount, cb){
   var account = accountList[accountId];
   account.reservedCurrency2 += amount;
   account.reservedCurrency2 = util.Round(account.reservedCurrency2, config.currency2.constant);
+  if(cb){
+    cb(account);
+  }
 }
 
-function removeFromReservedCurrency2(accountId, amount){
-  addToReservedCurrency2(accountId, -1*amount);
+function removeFromReservedCurrency2(accountId, amount, cb){
+  addToReservedCurrency2(accountId, -1*amount, function(res){
+    if(cb){
+      cb(res);
+    }
+  });
 }
 
 function addNewAccount(newAccountObj, cb){
   accountList.push(newAccountObj);
-  cb(newAccountObj);
+  if(cb){ 
+    cb(newAccountObj);
+  }
 }
 
 exports.AddToReservedCurrency1 = addToReservedCurrency1;
