c15c933fff6d7ba511d61e3c2fe3ab3f0a3f7153
Peter Munnings <petermunnings@gmail.com>
2017-01-12 12:43:39 +0200

    Added some login functionality

--- WebClient/src/app/login/login.component.ts
+++ WebClient/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 ﻿import { Component } from '@angular/core';
+import { AlertService, AuthenticationService } from '../_services/index';
 
 @Component({
   selector: 'my-login-component',
@@ -7,21 +8,25 @@
 
 export class LoginComponent {
   model: any = {};
+  loading: boolean = false;
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private alertService: AlertService
+  ){}
 
   login() {
     console.log('login clicked');
-    /*
     this.loading = true;
     this.authenticationService.login(this.model.username, this.model.password)
       .subscribe(
         data => {
-          this.router.navigate([this.returnUrl]);
+          //this.router.navigate([this.returnUrl]);
           console.log('login result: ', data);
         },
         error => {
           this.alertService.error(error);
           this.loading = false;
         });
-   */ 
   }
 }
